d2d221c72d0b48ee1001ea2e3cae4f115b3667b7
KIU Shueng Chuan <nixchuan@gmail.com>
2014-08-03 03:54:25 +0800

    change test code to use a pair of zsocks

--- ev_zsock_test.c
+++ ev_zsock_test.c
@@ -17,20 +17,17 @@ sigint_cb(struct ev_loop *loop, ev_signal *w, int revents)
 void timeout_cb (struct ev_loop *loop, ev_timer *w, int revents)
 {
 	ev_tstamp ts = ev_now (loop);
-	printf("%.3f\n", ts);
+	void *zsock = w->data;
+	zmq_send(zsock, &ts, sizeof(ts), 0);
 }
 
 void zsock_cb(struct ev_loop *loop, ev_zsock_t *wz, int revents)
 {
-	char text[64] = {0};
+	ev_tstamp ts_recv = ev_now (loop);
+	ev_tstamp ts_send;
+	zmq_recv(wz->zsock, &ts_send, sizeof(ts_send), 0);
 
-	zmq_msg_t msg;
-	zmq_msg_init(&msg);
-	zmq_msg_recv(&msg, wz->zsock, 0);
-	if (zmq_msg_size(&msg) < sizeof(text))
-		memcpy(text, zmq_msg_data(&msg), zmq_msg_size(&msg));
-	zmq_msg_close(&msg);
-	printf("%s\n", text);
+	printf("%f\n", ts_recv - ts_send);
 }
 
 int main()
@@ -41,25 +38,32 @@ int main()
 	ev_signal_init(p_signal_watcher, sigint_cb, SIGINT);
 	ev_signal_start(loop, &signal_watcher);
 
-	ev_timer timeout_watcher;
-	ev_timer *p_timeout_watcher = &timeout_watcher;
-	ev_timer_init (p_timeout_watcher, timeout_cb, 1.0, 1.0);
-	ev_timer_start (loop, &timeout_watcher);
-
 	void *zctx = zmq_ctx_new();
-	void *zsock = zmq_socket(zctx, ZMQ_PULL);
-	assert(zsock!=NULL);
-	int rc = zmq_bind(zsock, "tcp://127.0.0.1:5555");
+	void *zsock_recv = zmq_socket(zctx, ZMQ_PULL);
+	assert(zsock_recv!=NULL);
+	int rc = zmq_bind(zsock_recv, "inproc://channel");
 	assert(rc!=-1);
 
 	ev_zsock_t wz;
-	ev_zsock_init(&wz, zsock_cb, zsock, ZMQ_POLLIN);
+	ev_zsock_init(&wz, zsock_cb, zsock_recv, ZMQ_POLLIN);
 	ev_zsock_start(loop, &wz);
+
+	void *zsock_send = zmq_socket(zctx, ZMQ_PUSH);
+	assert(zsock_send!=NULL);
+	rc = zmq_connect(zsock_send, "inproc://channel");
+	assert(rc!=-1);
+
+	ev_timer timeout_watcher;
+	ev_timer *p_timeout_watcher = &timeout_watcher;
+	ev_timer_init (p_timeout_watcher, timeout_cb, 1.0, 1.0);
+	timeout_watcher.data = zsock_send;
+	ev_timer_start (loop, &timeout_watcher);
 
 	ev_run (loop, 0);
 	printf("loop exited\n");
 
-	zmq_close(zsock);
+	zmq_close(zsock_recv);
+	zmq_close(zsock_send);
 	zmq_ctx_destroy(zctx);
 
 	return 0;
